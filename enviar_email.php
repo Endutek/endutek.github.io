76416472e87cb0d7746b226a9f01f7f6cafaeb0f
johnylab <joaobatistacomunic@hotmail.com>
2016-12-22 11:14:22 -0200

    Correção de erros ao enviar email

--- enviar_email.php
+++ enviar_email.php
@@ -2,7 +2,7 @@
 
 $my_domain = "endutek.com.br";
 
-if ($_POST['nome'] !== '' && $_POST['email'] !== '' && $_POST['telefone'] !== '' && $_POST['honeypot'] === '') {
+if ($_POST['nome'] !== '' && $_POST['email'] !== '' && $_POST['telefone'] !== '' && $_POST['honey'] === '') {
 	$nome = $_POST['nome'];
 	$email = $_POST['email'];
 	$telefone = $_POST['telefone'];
@@ -15,21 +15,29 @@ if ($_POST['nome'] !== '' && $_POST['email'] !== '' && $_POST['telefone'] !== ''
 	$email_conteudo = "Nome: $nome \n";
 	$email_conteudo .= "Email: $email \n";
 	$email_conteudo .= "Telefone: $telefone \n";
+	//Orçamento
+	if (is_array($_POST['produtos'])) {
+		$produtos = $_POST['produtos'];
+		foreach ($produtos as $nome => $valor) {
+			echo "<p>$nome $valor</p>";
+		}
+		exit('array detectado');
+	}
 	$email_conteudo .= "\n=== Mensagem ===\n\n$mensagem \n";
 	//Seta os Headers (Alterar somente caso necessario)
 	$email_headers = implode ( "\n",array ( "From: $email_remetente", "Reply-To: $email_reply", "Return-Path: $email_remetente","MIME-Version: 1.0","X-Priority: 3","Content-Type: text/html; charset=UTF-8" ) );
 	//Enviando o email
 	if (mail ($email_destinatario, $email_assunto, nl2br($email_conteudo), $email_headers)){
-		header('Location: http://' . $my_domain . '/email_enviado.html');
+		// header('Location: http://' . $my_domain . '/email_enviado.html');
 		exit('email enviado');
 	}
 	else{
-		header('Location: http://' . $my_domain . '/email_com_erro.html');
+		// header('Location: http://' . $my_domain . '/email_com_erro.html');
 		exit('email com erro');
 	}
 }
 else {
-	header('Location: http://' . $my_domain . '/email_incompleto.html');
+	// header('Location: http://' . $my_domain . '/email_incompleto.html');
 	exit('dados faltando');
 }
 ?>
